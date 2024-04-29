2bbd1ad492426a4a1edef8f7f1fb2599c3089196
Brian Job Okello <63491479+BrianPollar@users.noreply.github.com>
2024-04-29 12:04:40 +0300

    gitignore adjustent

---
+++ packages/stock-notif-server/lib/cjs/stock-notif-server/vite.config.js
@@ -0,0 +1,12 @@
+"use strict";
+/* eslint-disable spaced-comment */
+/// <reference types="vitest" />
+Object.defineProperty(exports, "__esModule", { value: true });
+const vite_1 = require("vite");
+exports.default = (0, vite_1.defineConfig)({
+    test: {
+    /* for example, use global to avoid globals imports (describe, test, expect): */
+    // globals: true,
+    }
+});
+//# sourceMappingURL=vite.config.js.map
